2d09646d50b4c8146710f60fcc9e3b67c0455fce
Milan Lacmanovic <xg023885@live.reading.ac.uk>
2018-11-20 12:42:07 +0000

    To Finish

--- empTask6W3 TODO.sql
+++ empTask6W3 TODO.sql
@@ -1,1 +1,5 @@
-SELECT e.empno, d.dname FROM emp e JOIN dept d ON e.deptno = d.deptno WHERE (SELECT MIN(sal) FROM emp GROUP BY deptno);
+--SELECT e.empno, d.dname FROM emp e JOIN dept d ON e.deptno = d.deptno WHERE e.empno = (SELECT empno FROM emp GROUP BY deptno ); --Bad
+
+--SELECT deptno, MIN(sal) FROM emp GROUP BY deptno; --Good
+
+--SELECT e.empno, d.dname, MIN(e.sal) FROM emp e JOIN dept d ON e.deptno = d.deptno GROUP BY d.deptno; --Close
